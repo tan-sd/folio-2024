b8a92fc905dc7210962f3b0ec1a4b0bae70784d0
tan-sd <shengdatan@gmail.com>
2024-02-09 10:23:12 +0200

    Intialise index

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Experience from "./Experience";
 import "./main.css";
+import { Canvas } from "@react-three/fiber";
+import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Experience />
+        <Navbar />
+        <div className="canvas-container">
+            <Canvas
+                shadows
+                gl={{ antialias: false }}
+                dpr={[1, 1.5]}
+                camera={{ position: [0, 0, 20], fov: 35, near: 1, far: 40 }}
+            >
+                <Experience />
+            </Canvas>
+        </div>
+        <Footer />
     </React.StrictMode>
 );
